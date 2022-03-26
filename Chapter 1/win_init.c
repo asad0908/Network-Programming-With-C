bdcc6e9e8c0ee463e4a1859046bedb8267fcd2a2
Asad Memon <amdeveloping0908@gmail.com>
2022-03-26 23:26:42 +0530

    Chapter 1 programs completed

---
+++ Chapter 1/win_init.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <winsock.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d))
+    {
+        printf("Failed to initialize.\n");
+        return -1;
+    }
+    WSACleanup();
+    printf("Ok.\n");
+    return 0;
+}
